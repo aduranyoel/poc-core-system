d216b4cb2a9468419bf4fbdfac853d97e481703f
Maday Concepcion <yoet92@gmail.com>
2022-07-01 15:09:37 -0300

    Updates

---
+++ precache-manifest.4c3624ebfaf318267bdf124114131f19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453d36c8b209d7358bb39c6841963a87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c3ec5ff94d2e04d5e505",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "d94c1916b4f1ba806654",
+    "url": "./static/js/2.f4d07606.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f4d07606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ec5ff94d2e04d5e505",
+    "url": "./static/js/main.09d5a41f.chunk.js"
+  },
+  {
+    "revision": "93ca23197ff883f76b2d",
+    "url": "./static/js/runtime-main.e55e52e0.js"
+  }
+]);
